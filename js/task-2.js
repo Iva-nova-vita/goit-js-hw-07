87b15ea3974833cffa60328f2fe68198377c53a2
v.ivanova <viktoriia.ivanova@vista.com>
2024-04-07 13:00:25 +0300

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -10,5 +10,21 @@ const images = [
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
-  }
+  },
 ];
+
+images.forEach((image) => {
+  const ul = document.querySelector('.gallery');
+  // first way
+
+  // const li = document.createElement('li');
+  // ul.append(li);
+  // const img = document.createElement('img');
+  // img.src = image.url;
+  // img.alt = image.alt;
+  // li.append(img);
+
+  // second way
+  const li = `<li><img src="${image.url}" alt="${image.alt}"</li>`
+  ul.insertAdjacentHTML("afterbegin", li);
+});
