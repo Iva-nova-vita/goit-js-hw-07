9b51c539916c3b21662336376cba4c586c109d49
v.ivanova <viktoriia.ivanova@vista.com>
2024-04-07 22:32:53 +0300

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,12 @@
+const btnChangeColor = document.querySelector('.change-color');
+btnChangeColor.addEventListener('click', changeColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+  document.querySelector('.color').textContent = color;
+  document.querySelector('body').style.backgroundColor =color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
